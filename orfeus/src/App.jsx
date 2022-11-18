3eebe5c83be11f9d6432b35f78e73afdd323bf02
mel-san <57415626+mel-san@users.noreply.github.com>
2022-11-18 15:39:28 -0800

    login page

---
+++ orfeus/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+import Login from './login/Login'
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className= "sections">
+        <Login/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
