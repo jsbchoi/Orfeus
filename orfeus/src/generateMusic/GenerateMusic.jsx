d44684a6045af99034a59284fc6bb81ae991dc84
yongyi-z <yongyiz@nevada.unr.edu>
2022-11-18 23:54:02 -0800

    setup

---
+++ orfeus/src/generateMusic/GenerateMusic.jsx
@@ -0,0 +1,18 @@
+import "./generateMusic.css";
+
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+export default class GenerateMusic extends Component{
+
+
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ orfeus/src/publicLibrary/PublicLibrary.jsx
@@ -0,0 +1,18 @@
+import "./publicLibrary.css";
+
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+export default class PublicLibrary extends Component{
+
+
+
+
+
+
+
+
+
+
+
+    
+}
