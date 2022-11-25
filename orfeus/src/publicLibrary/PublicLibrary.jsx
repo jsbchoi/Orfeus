bed56cf75757750adb01f376190d9d4621281981
yongyi-z <yongyiz@nevada.unr.edu>
2022-11-24 16:03:02 -0800

    inputs + search bar

--- orfeus/src/publicLibrary/PublicLibrary.jsx
+++ orfeus/src/publicLibrary/PublicLibrary.jsx
@@ -1,19 +1,54 @@
 import "./publicLibrary.css";
-
-import React, { Component } from "react";
+import Search from './search';
+import React, { Component, useState} from "react";
 import { Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const example = [
+    { id: '1', name: 'username1' },
+    { id: '2', name: 'audiofile name 394' },
+    { id: '3', name: 'username28' },
+    { id: '4', name: 'audio file' },
+];
+
+const filterPosts = (posts, query) => {
+    if (!query) {
+        return posts;
+    }
+
+    return posts.filter((post) => {
+        const postName = post.name.toLowerCase();
+        return postName.includes(query);
+    });
+};
+
+const { search } = window.location;
+const query = new URLSearchParams(search).get('s');
+const [searchQuery, setSearchQuery] = useState(query || '');    
+const filteredPosts = filterPosts(example, searchQuery);
+
 export default class PublicLibrary extends Component{
+    
     render(){
-        return(
+        return (
+            <Router>
             <form>
                 <h2>Library</h2>
-
-
+                <Search 
+                    searchQuery={searchQuery}                
+                    setSearchQuery={setSearchQuery}
+                />
+                <ul>
+                    {filteredPosts.map((example) => (                    
+                        <li key={example.id}>{example.name}</li>
+                    ))}
+                </ul>
 
 
 
 
             </form>
+            </Router>
         )
     }
 }
