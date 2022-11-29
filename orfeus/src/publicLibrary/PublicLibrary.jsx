82828ffd2fc96fffc44209a9c6b8d9ded4d2846b
yongyi-z <yongyiz@nevada.unr.edu>
2022-11-29 13:32:30 -0800

    searchbar works

--- orfeus/src/publicLibrary/PublicLibrary.jsx
+++ orfeus/src/publicLibrary/PublicLibrary.jsx
@@ -1,25 +1,48 @@
-import "./publicLibrary.css";
-import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import SearchBar from "./SearchBar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './publicLibrary.css';
+import Search from './search';
 
+const examples = [
+    { id: '1', name: 'username1' },
+    { id: '2', name: 'audiofile name 394' },
+    { id: '3', name: 'username28' },
+    { id: '4', name: 'audio file' },
+];
 
-export default class PublicLibrary extends Component{
-    
-    render(){
-        return (
-            <form>
-                <h2>Library</h2>
-                <div>
-                    <SearchBar/>
-                </div>
-                
-
-
-
-
-            </form>
-        )
+const filterExamples = (examples, query) => {
+    if (!query) {
+        return examples;
     }
+
+    return examples.filter((example) => {
+        const exampleName = example.name.toLowerCase();
+        return exampleName.includes(query);
+    });
+};
+
+const App = () => {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+    const filteredExamples = filterExamples(examples, searchQuery);
+    return (
+        //<Router>
+            <div className='Library'>
+                <h2>Public Library</h2>
+                <Search 
+                    searchQuery={searchQuery}                
+                    setSearchQuery={setSearchQuery}
+                />
+                <ul>
+                    {filteredExamples.map(example => (                    
+                        <li key={example.key}>{example.name}</li>
+                    ))}
+                </ul>
+            </div>
+        //</Router>
+    );
 }
+
+export default App;
