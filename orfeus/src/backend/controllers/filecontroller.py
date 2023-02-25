a701b8cba674ccc92873614be99dd2c6f6949d19
NamYoonsung <ynam@nevada.unr.edu>
2023-02-24 17:43:30 -0800

    library

--- orfeus/src/backend/controllers/filecontroller.py
+++ orfeus/src/backend/controllers/filecontroller.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from flask import Blueprint, make_response, request
 from flask_cors import cross_origin
 from flask_jwt_extended import decode_token
-from models import User, song_file
+from models import User, song_file, SongFile
 from orfeus_config import engine
 from sqlalchemy import select
 from scipy.io import wavfile
 import numpy as np
 import os, io
+import bcrypt, os, json
 
 
 file_bp = Blueprint('files', __name__)
@@ -63,6 +64,69 @@ def upload():
         response.status_code = 200
         return response
     return "flask server"
+
+@file_bp.route('/getFile', methods=['GET', 'OPTIONS'])
+@cross_origin()
+def getSongs():
+    # if request.method == 'GET':
+    #     retrieved_file = request.files["file"]
+    #     data = retrieved_file.read()
+    #     now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    #     # Get the username from the decoded token
+    #     access_token = request.headers.get('Authorization')
+    #     decoded_token = decode_token(access_token.replace("Bearer", "").strip())
+    #     user = decoded_token['sub']
+
+    #     # # Create the output directory if it doesn't exist
+    #     output_dir = os.path.join(os.getcwd(), 'song_database', user)
+    #     os.makedirs(output_dir, exist_ok=True)  
+
+    #     # Save the file as a WAV file
+    #     output_filename = f"{user}-{now}.wav"
+    #     output_path = os.path.join(output_dir, output_filename)
+    #     print(output_path)
+    #     data_buffer = io.BytesIO(data)
+    #     audio_data = np.frombuffer(data_buffer.read(), dtype=np.int16)
+    #     wavfile.write(output_path, 44100, audio_data)
+        
+    #     #Get user id to store file path and user id in database
+    #     user_id_query = select(User.id).where(User.username == user)
+    #     conn = engine.connect()
+    #     idResult = conn.execute(user_id_query)
+    #     dictionaryForQueryResult = {}
+    #     for row in idResult:
+    #         dictionaryForQueryResult = row._mapping
+    #     insert_query = song_file.insert().values(title=output_filename, 
+    #                                              filepath=output_path, 
+    #                                              uploaded_date=str(now), 
+    #                                              genre_id=1, artist_id=1, 
+    #                                              user_id=dictionaryForQueryResult['id'])
+    #     connection = engine.connect()
+    #     result = connection.execute(insert_query)
+    #     print(result)
+    #     connection.close()
+    #     return make_response("file saved in " + output_path, 200)
+
+    # elif request.method == 'OPTIONS':
+    #     # respond to the preflight request
+    #     response = make_response()
+    #     response.headers.add('Access-Control-Allow-Origin', '*')
+    #     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    #     response.headers.add('Access-Control-Allow-Methods', 'POST')
+    #     response.status_code = 200
+    #     return response
+    # return "flask server"
+    songs = SongFile.query.all()
+    song_list = []
+    for song in songs:
+        song_dict = {
+            column.name: getattr(song, column.name)
+            for column in song.__table__.columns
+            if column.name != "InstanceState"
+        }
+        song_list.append(song_dict)
+    return json.dumps(song_list, default=str)
+
 
 
 
--- orfeus/src/views/publicLibrary/PublicLibrary.jsx
+++ orfeus/src/views/publicLibrary/PublicLibrary.jsx
@@ -1,45 +1,77 @@
 //Search Bar code: https://www.emgoto.com/react-search-bar/
 
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import library_styles from './PublicLibrary.module.css';
-import Search from './search';
+import { Link } from "react-router-dom";
+import SongList from "./SongDB";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import library_styles from "./PublicLibrary.module.css";
+import Search from "./search";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const baseURL = "http://127.0.0.1:5000/";
 
 const examples = [
-  { id: '1', name: 'username1' },
-  { id: '2', name: 'audiofile name 394' },
-  { id: '3', name: 'username28' },
-  { id: '4', name: 'audio file' },
+  { id: "1", name: "songname1" },
+  { id: "2", name: "audiofile name 394" },
+  { id: "3", name: "songname28" },
+  { id: "4", name: "audio file" },
 ];
 
-const filterExamples = (examples, query) => {
-  if (!query) {
-    return examples;
+const MusicDB = () => {
+  const navigate = useNavigate();
+  const [songs, setSongs] = useState([]);
+
+  function fetchData() {
+    return axios
+      .get(baseURL + "getFile", {})
+      .then((response) => response.data)
+      .catch((error) => console.error(error));
   }
 
-  return examples.filter((example) => {
-    const exampleName = example.name.toLowerCase();
-    return exampleName.includes(query);
+  useEffect(() => {
+    fetchData()
+      .then((data) => {
+        console.log(data);
+        setSongs(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  return songs;
+};
+
+const filterExamples = (SongList, query) => {
+  const songs = MusicDB();
+  if (!query) {
+    return songs;
+  }
+
+  return songs.filter((song) => {
+    const songName = song.title.toLowerCase();
+    return songName.includes(query);
   });
 };
 
-const App = () => {
+const Library = () => {
+  const songs = MusicDB();
   const { search } = window.location;
-  const query = new URLSearchParams(search).get('s');
-  const [searchQuery, setSearchQuery] = useState(query || '');
-  const filteredExamples = filterExamples(examples, searchQuery);
+  const query = new URLSearchParams(search).get("s");
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const filteredExamples = filterExamples(songs, searchQuery);
   return (
     <div className={library_styles.Library}>
       <h2>Public Library</h2>
       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <ul>
-        {filteredExamples.map((example) => (
-          <li key={example.key}>{example.name}</li>
+        {filteredExamples.map((songs) => (
+          <li key={songs.song_id}>
+            {songs.title}
+            {songs.artist_id}
+            {songs.genre_id}
+          </li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default App;
+export default Library;
