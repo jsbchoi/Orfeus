31398d3c0f6e16c485e9356c6a149e724b4efbf2
mel-san <57415626+mel-san@users.noreply.github.com>
2023-02-25 16:20:44 -0800

    card layout

--- orfeus/src/publicLibrary/PublicLibrary.jsx
+++ orfeus/src/publicLibrary/PublicLibrary.jsx
@@ -1,48 +1,52 @@
 //Search Bar code: https://www.emgoto.com/react-search-bar/
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+
 import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './publicLibrary.css';
 import Search from './search';
 
 const examples = [
-    { id: '1', name: 'username1' },
-    { id: '2', name: 'audiofile name 394' },
-    { id: '3', name: 'username28' },
-    { id: '4', name: 'audio file' },
+  { id: '1', name: 'username1' },
+  { id: '2', name: 'audiofile name 394' },
+  { id: '3', name: 'username28' },
+  { id: '4', name: 'audio file' },
 ];
 
 const filterExamples = (examples, query) => {
-    if (!query) {
-        return examples;
-    }
+  if (!query) {
+    return examples;
+  }
 
-    return examples.filter((example) => {
-        const exampleName = example.name.toLowerCase();
-        return exampleName.includes(query);
-    });
+  return examples.filter((example) => {
+    const exampleName = example.name.toLowerCase();
+    return exampleName.includes(query);
+  });
 };
 
 const Library = () => {
-    const { search } = window.location;
-    const query = new URLSearchParams(search).get('s');
-    const [searchQuery, setSearchQuery] = useState(query || '');
-    const filteredExamples = filterExamples(examples, searchQuery);
-    return (
-            <div className='Library'>
-                <h2>Public Library</h2>
-                <Search 
-                    searchQuery={searchQuery}                
-                    setSearchQuery={setSearchQuery}
-                />
-                <ul>
-                    {filteredExamples.map(example => (                    
-                        <li key={example.key}>{example.name}</li>
-                    ))}
-                </ul>
-            </div>
-    );
-}
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');
+  const filteredExamples = filterExamples(examples, searchQuery);
+  return (
+    <div className="Library">
+      <h2>Public Library</h2>
+      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <ul>
+        {filteredExamples.map((example) => (
+          <li key={example.key}>{example.name}</li>
+        ))}
+      </ul>
+      <Card bg="light-blue">
+        <Card.Body>
+          <Card.Text>foobar</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
 export default Library;
--- orfeus/src/views/publicLibrary/PublicLibrary.jsx
+++ orfeus/src/views/publicLibrary/PublicLibrary.jsx
@@ -1,20 +1,31 @@
 //Search Bar code: https://www.emgoto.com/react-search-bar/
 
-import { Link } from "react-router-dom";
-import SongList from "./SongDB";
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import library_styles from "./PublicLibrary.module.css";
-import Search from "./search";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-const baseURL = "http://127.0.0.1:5000/";
+import { Link } from 'react-router-dom';
+import SongList from './SongDB';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import library_styles from './PublicLibrary.module.css';
+import Search from './search';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+const baseURL = 'http://127.0.0.1:5000/';
 
 const examples = [
-  { id: "1", name: "songname1" },
-  { id: "2", name: "audiofile name 394" },
-  { id: "3", name: "songname28" },
-  { id: "4", name: "audio file" },
+  { id: '1', name: 'songname1' },
+  { id: '2', name: 'audiofile name 394' },
+  { id: '3', name: 'songname28' },
+  { id: '4', name: 'audio file' },
+];
+
+const top_songs = [
+  { id: '1', name: 'Top Song 1' },
+  { id: '2', name: 'Top Song 2' },
+  { id: '3', name: 'Top Song 3' },
+  { id: '4', name: 'Top Song 4' },
+  { id: '5', name: 'Top Song 5' },
 ];
 
 const MusicDB = () => {
@@ -23,7 +34,7 @@ const MusicDB = () => {
 
   function fetchData() {
     return axios
-      .get(baseURL + "getFile", {})
+      .get(baseURL + 'getFile', {})
       .then((response) => response.data)
       .catch((error) => console.error(error));
   }
@@ -54,12 +65,26 @@ const filterExamples = (SongList, query) => {
 const Library = () => {
   const songs = MusicDB();
   const { search } = window.location;
-  const query = new URLSearchParams(search).get("s");
-  const [searchQuery, setSearchQuery] = useState(query || "");
+  const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');
   const filteredExamples = filterExamples(songs, searchQuery);
   return (
     <div className={library_styles.Library}>
       <h2>Public Library</h2>
+      {/* <Card bg="secondary">
+        <Card.Body>
+          <Card.Text>Top Song 1</Card.Text>
+        </Card.Body>
+      </Card> */}
+      <CardGroup>
+        {top_songs.map((song) => (
+          <Card bg="secondary">
+            <Card.Body>
+              <Card.Text>{song.name}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </CardGroup>
       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <ul>
         {filteredExamples.map((songs) => (
