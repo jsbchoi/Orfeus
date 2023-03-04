1c1887ffedefbcc883b75005fd298ec8e4f8ac69
Justin Choi <39545541+jsbchoi@users.noreply.github.com>
2023-03-03 16:20:26 -0800

    Some backend tests
    - Implemented unit tests for login, register, and delete API routes.

--- orfeus/src/tests/test_signup.py
+++ orfeus/src/tests/test_signup.py
@@ -14,7 +14,7 @@ def app():
 def client(app):
     return app.test_client()
 
-def test_register():
+def test_register_and_delete():
     # Send a POST request with valid credentials
     data = [{"id": "1", "value": "testsignupusername"}, 
             {"id": "2", "value": "testsignuppassword"}, 
@@ -29,3 +29,17 @@ def test_register():
     assert response.status_code == 200
     # Check that the response contains an access tokendata
     assert 'access_token' in response.json()
+     
+    # Clean up by testing delete
+    url = baseURL + 'users/testsignupusername'
+    response = requests.delete(url)
+    print(f"Sending request to {url} with delete request")
+    print(f"Response status code: {response.status_code}")
+    print(f"Response data: {response.text}")
+    
+    # Check that the response status code is 200 OK
+    assert response.status_code == 200
+    # Check that the response says that the user was deleted 
+    assert response.text == "User deleted"
+
+    
