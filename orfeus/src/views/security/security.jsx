e58704bd1460b1a26ba5cb52a398087798037405
NamYoonsung <ynam@nevada.unr.edu>
2023-03-07 18:57:02 -0800

    pop up for change pass

--- orfeus/src/views/security/security.jsx
+++ orfeus/src/views/security/security.jsx
@@ -1,29 +1,31 @@
-import security_styles from './security.module.css';
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
+import security_styles from "./security.module.css";
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const baseURL = 'http://127.0.0.1:5000/';
+const baseURL = "http://127.0.0.1:5000/";
 
 const Security = () => {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState(' ');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const token = localStorage.getItem('access_token');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const token = localStorage.getItem("access_token");
 
   useEffect(() => {
-    const token = localStorage.getItem('access_token');
+    const token = localStorage.getItem("access_token");
     if (!token) {
-      navigate('/Login');
+      navigate("/Login");
     } else {
       const decodedToken = jwt_decode(token);
-      setUsername(decodedToken['sub']);
+      setUsername(decodedToken["sub"]);
     }
   }, [navigate]);
 
@@ -39,16 +41,18 @@ const Security = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
+      toast.error("Passwords do not match!");
       console.log("Passwords don't match");
       return;
     }
     try {
-      const response = await axios.put(baseURL + 'users/' + username, {
+      const response = await axios.put(baseURL + "users/" + username, {
         email: email,
         password: password,
       });
       console.log(response.data);
-      navigate('/account');
+      toast.success("Password changed successfully");
+      navigate("/account/security");
     } catch (error) {
       console.error(error);
     }
@@ -56,7 +60,7 @@ const Security = () => {
 
   function fetchData() {
     return axios
-      .get(baseURL + 'users', {
+      .get(baseURL + "users", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -67,10 +71,11 @@ const Security = () => {
 
   return (
     <body className={security_styles.profile_body}>
+      <ToastContainer />
       <section className={security_styles.security_class}>
         <h1 className={security_styles.security_h1}>Security</h1>
         <Form className={security_styles.security_form}>
-          <Form.Group  controlId="name">
+          <Form.Group controlId="name">
             <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
@@ -79,7 +84,7 @@ const Security = () => {
               required
             ></Form.Control>
           </Form.Group>
-          <Form.Group  controlId="password">
+          <Form.Group controlId="password">
             <Form.Label>Enter New Password</Form.Label>
             <Form.Control
               type="password"
@@ -88,7 +93,7 @@ const Security = () => {
             />
           </Form.Group>
 
-          <Form.Group  controlId="password">
+          <Form.Group controlId="password">
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
               type="password"
