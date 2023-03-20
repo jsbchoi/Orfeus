8cd0892cd026115d51dba3ef850e6aea16f19c85
NamYoonsung <ynam@nevada.unr.edu>
2023-03-20 15:18:37 -0700

    new navbar

--- orfeus/src/views/root/Root.jsx
+++ orfeus/src/views/root/Root.jsx
@@ -14,8 +14,8 @@ import AdbIcon from "@mui/icons-material/Adb";
 //for the mui for navbar/appbar
 import { useState, useEffect } from "react";
 import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
-import root_styles from "./Root.module.css";
 import jwt_decode from "jwt-decode";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
 
 const pages = ["Home", "About", "Library", "Login", "Signup"];
 
@@ -56,6 +56,18 @@ const Root = () => {
   };
   const handleSignuppageClick = () => {
     navigate("/signup");
+  };
+  const handleGeneratepageClick = () => {
+    navigate("/generate");
+  };
+  const handleProfilepageClick = () => {
+    navigate("/account");
+  };
+  const handleLogoutpageClick = () => {
+    localStorage.removeItem("access_token");
+    setLoggedIn(false);
+    setDecodedToken(null);
+    navigate("/");
   };
 
   return (
@@ -99,23 +111,96 @@ const Root = () => {
               >
                 Library
               </Button>
-              <Button
-                sx={{
-                  my: 2,
-                  color: "white",
-                  display: "block",
-                  justifySelf: "flex-end",
-                }}
-                onClick={handleLoginpageClick}
-              >
-                Login
-              </Button>
-              <Button
-                sx={{ my: 2, color: "white", display: "block" }}
-                onClick={handleSignuppageClick}
-              >
-                Signup
-              </Button>
+              {loggedIn ? (
+                <>
+                  <Button
+                    sx={{ my: 2, color: "white", display: "block" }}
+                    onClick={handleGeneratepageClick}
+                  >
+                    Generate Music
+                  </Button>
+                  <Button
+                    sx={{ my: 2, color: "white", display: "block" }}
+                    onClick={handleProfilepageClick}
+                  >
+                    Profile
+                  </Button>
+                  <Box
+                    sx={{
+                      flexGrow: 0,
+                      marginLeft: "auto",
+                      placeSelf: "center end",
+                    }}
+                  >
+                    {decodedToken && (
+                      <div
+                        style={{
+                          color: "white",
+                          display: "flex-inline",
+                          flexDirection: "row",
+                        }}
+                      >
+                        {/* <a
+                          href="/account"
+                          style={{
+                            display: "inline-block",
+                            verticalAlign: "middle",
+                            color: "white",
+                          }}
+                        >
+                          {decodedToken.sub}
+                        </a> */}
+                        <Typography
+                          component="a"
+                          onClick={handleProfilepageClick}
+                          sx={{
+                            mr: 2,
+                            color: "inherit",
+                            textDecoration: "none",
+                          }}
+                        >
+                          {decodedToken.sub}
+                        </Typography>
+                        <SentimentVerySatisfiedIcon
+                          onClick={handleProfilepageClick}
+                        />
+                      </div>
+                    )}
+                  </Box>
+                  <Button
+                    sx={{
+                      my: 2,
+                      color: "white",
+                      display: "block",
+                      // marginLeft: "auto",
+                    }}
+                    onClick={handleLogoutpageClick}
+                  >
+                    Log Out
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    sx={{
+                      my: 2,
+                      color: "white",
+                      display: "block",
+                      justifySelf: "flex-end",
+                      marginLeft: "auto",
+                    }}
+                    onClick={handleLoginpageClick}
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    sx={{ my: 2, color: "white", display: "block" }}
+                    onClick={handleSignuppageClick}
+                  >
+                    Signup
+                  </Button>
+                </>
+              )}
             </Box>
           </Toolbar>
         </Container>
