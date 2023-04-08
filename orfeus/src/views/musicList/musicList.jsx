d24ae4a6eac90e4098a69df2b027d2aa394bc657
NamYoonsung <ynam@nevada.unr.edu>
2023-04-08 14:00:58 -0700

    scrollbar profile page fixed

--- orfeus/src/views/musicList/musicList.jsx
+++ orfeus/src/views/musicList/musicList.jsx
@@ -54,7 +54,7 @@ function MusicList() {
     },
   ];
   return (
-    <div className={music_styles.profile_body}>
+    <div className={music_styles.profile_body} style={{ overflow: 'hidden', height: '100%' }}>
       <h1 className={music_styles.music_h1}>List of your music</h1>
       <Box
         sx={{
