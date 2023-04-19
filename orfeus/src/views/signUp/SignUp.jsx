779aeaa19c6ca53a8095e3eb7aa09ab10673ff2e
yongyi-z <yongyiz@nevada.unr.edu>
2023-04-18 23:32:15 -0700

    long+signup ui

--- orfeus/src/views/signUp/SignUp.jsx
+++ orfeus/src/views/signUp/SignUp.jsx
@@ -1,10 +1,21 @@
 import signUp_styles from "./SignUp.module.css";
 import Form from "./Form";
-
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 export default class SignUp extends Component {
   render() {
+
+    const ColorButton = styled(Button)(({ theme }) => ({
+      color: theme.palette.getContrastText(purple[500]),
+      backgroundColor: purple[400],
+      '&:hover': {
+        backgroundColor: purple[600],
+      },
+    }));
+
     return (
       <div className={signUp_styles.div_topForm_signup}>
         <form className={signUp_styles.topForm_signup}>
@@ -13,14 +24,20 @@ export default class SignUp extends Component {
           <div className={signUp_styles.wholeForm_signup}>
             <Form />
           </div>
-          <p className={signUp_styles.tail_signup}>
-            <Link to="/Login" className={signUp_styles.already_registered}>
-              Already Registered?
+          <div>
+            <Link to="/Login">
+              <ColorButton
+                variant="contained"
+                color="success"
+              >
+                Already Registered?
+              </ColorButton>
             </Link>
+          </div>
             {/* <Link to="/Login">
               <button type="login">Login</button>
             </Link> */}
-          </p>
+          
         </form>
       </div>
     );
